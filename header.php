17879b9cbfcbe24b0e8042b32d0afecae366cc9d
Notosound <jonnathanpena1@gmail.com>
2024-07-08 09:43:51 -0500

    main
    Añadir archivos iniciales para la página web

--- header.php
+++ header.php
@@ -4,13 +4,12 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+  <link rel="stylesheet" href="styles.css">
   <title>Lupaper</title>
-  <?php wp_head(); ?>
 </head>
 <body>
   <header class="header">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="Lupaper Logo" class="logo">
+    <img src="images/logo.png" alt="Lupaper Logo" class="logo">
     <h1>Lupaper</h1>
     <nav class="navbar">
       <a href="#home">Inicio</a>

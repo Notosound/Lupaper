7ca545704514d3a60b700de78877295b49286b33
Notosound <jonnathanpena1@gmail.com>
2024-07-08 10:02:39 -0500

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
   <footer class="footer">
     <p>&copy; 2024 Lupaper. Todos los derechos reservados.</p>
   </footer>
-  <?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -4,12 +4,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="styles.css">
+  <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
   <title>Lupaper</title>
+  <?php wp_head(); ?>
 </head>
 <body>
   <header class="header">
-    <img src="images/logo.png" alt="Lupaper Logo" class="logo">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="Lupaper Logo" class="logo">
     <h1>Lupaper</h1>
     <nav class="navbar">
       <a href="#home">Inicio</a>
